e38df96991a822f26e4a257ebcc052a8f908957a
CRC-web <watsonkumar86@gmail.com>
2026-03-14 13:43:07 -0400

    json test2

--- scripts/action.js
+++ scripts/action.js
@@ -72,11 +72,17 @@ function LoadPage()
         pg2.hidden = true;
         pg3.hidden = false;
 
-        fetch('../media/json/email.json')
+        fetch('../../media/json/email.json')
             .then((response) => response.json())
+            .then((json) => {
+                const url = document.URL;
+                const topic = url.split("Take-Action/")[1].split("/")[0];
+
+                const email = json[topic];
+                console.log(email);
+            });
         
         console.log(document.URL);
-        console.log(response);
 
         const emails = "ffarry@pasen.gov,boscola@pasenate.com,kward@pasen.gov,rbrown@pasen.gov,lculver@pasen.gov,cgebhard@pasen.gov,flynn@pasenate.com,PASenatorNick@pasenate.com,senatorsantarsiero@pasenate.com,tartaglione@pasenate.com,mbrooks@pasen.gov,pstefano@pasen.gov";
         const finalbody = `Dear Senator,\n\nI am writing to ask you to support HB 1512 and vote to report it favorably out of committee. The bill would require manufacturers to supply Pennsylvanians with the proper tools, documentation, and reasonable access to repair their own digitally-based products.\n\n${bodymsg}\n\nAs a resident of Pennsylvania, I urge the committee to allow the full Senate to vote on the issue by reporting HB 1512 out of committee favorably.\n\nThank you for your time and consideration.\n\n${nameinp.value}\n${cityinp.value}, Pennsylvania, ${zipinp.value}`;
